38b5b0d09469b88c8183e09c5c631bff7f2577c6
Manuel Transfeld <auipga@gmail.com>
2023-03-19 21:40:55 +0100

    Extract outer keys

---
+++ config/outer-keys.h
@@ -0,0 +1,40 @@
+/*
+ * Aliases for outer keys
+*/
+
+// Left Outer Pinky
+#define LOP_1   &kp DE_GRAVE
+#define LOP_2   &kp TAB
+#define LOP_3   &kp LSHFT
+
+// Right Outer Pinky
+#define ROP_1   &kp BSPC
+#define ROP_2   &kp ENTER
+#define ROP_3   &kp RSHFT
+
+
+// Left Arrow Area
+#define LAR_1   &kp C_MUTE
+#define LAR_2   &kp ESC
+#define LAR_3   &kp LCTRL
+
+// Right Arrow Area
+#define RAR_0   &kp UP
+#define RAR_1   &kp LEFT
+#define RAR_2   &kp DOWN
+#define RAR_3   &kp RIGHT
+
+
+// Left Thumb Keys
+#define LT_1    &mo NAV_L
+#define LT_2    &sk LSHIFT
+#define LT_2_1  &sk DE_Z
+#define LT_3    &kp LALT
+#define LT_4    &kp LGUI
+
+// Right Thumb Keys
+#define RT_1    &mo SYM_L
+#define RT_2    &kp SPACE
+#define RT_2_1  &kp DE_FSLH
+#define RT_3    &kp LALT
+#define RT_4    &kp RGUI
