228997213a9ede618079978a3318f7f2b18d11cb
Manuel Transfeld <auipga@gmail.com>
2023-03-20 19:13:40 +0100

    Combos: fix shorter way

--- config/combos.h
+++ config/combos.h
@@ -1,5 +1,5 @@
 #define COMBO(KEYPOS1, KEYPOS2, KP) \
-    combo_##KEYPOS1_##KEYPOS2 { \
+    combo_##KEYPOS1 ## _ ##KEYPOS2 { \
         timeout-ms = <40>; \
         key-positions = <KEYPOS1 KEYPOS2>; \
         layers = <0>; \
