328bc2adbe8533d1b24bd02f7a6268e271e4eff6
Manuel Transfeld <auipga@gmail.com>
2023-03-23 01:06:05 +0100

    Enable symbol combos on other layers

--- config/combos.h
+++ config/combos.h
@@ -2,6 +2,6 @@
     combo_##KEYPOS1##_##KEYPOS2 { \
         timeout-ms = <40>; \
         key-positions = <KEYPOS1 KEYPOS2>; \
-        layers = <0>; \
+        layers = <DVORAK_L DK_HRM_L NUM_L>; \
         bindings = <&kp KP>; \
     };
