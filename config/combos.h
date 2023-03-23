b9196c78c9505851cd24885e92df4ff36f5a4841
Manuel Transfeld <auipga@gmail.com>
2023-03-23 01:24:36 +0100

    Add combos for Caps-Lock and &caps_word

--- config/combos.h
+++ config/combos.h
@@ -1,7 +1,17 @@
+// combo with binding to &kp
 #define COMBO(KEYPOS1, KEYPOS2, KP) \
     combo_##KEYPOS1##_##KEYPOS2 { \
         timeout-ms = <40>; \
         key-positions = <KEYPOS1 KEYPOS2>; \
         layers = <DVORAK_L DK_HRM_L NUM_L>; \
         bindings = <&kp KP>; \
+    };
+
+// combo with custom binding
+#define COMBC(KEYPOS1, KEYPOS2, BINDING) \
+    combo_##KEYPOS1##_##KEYPOS2 { \
+        timeout-ms = <40>; \
+        key-positions = <KEYPOS1 KEYPOS2>; \
+        layers = <DVORAK_L DK_HRM_L>; \
+        bindings = <BINDING>; \
     };
