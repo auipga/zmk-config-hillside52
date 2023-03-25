050542e19aeb29ec1f3aeaa06a2b388fe35712fb
Manuel Transfeld <auipga@gmail.com>
2023-03-25 16:36:38 +0100

    Toggle home row mods by using a combo

--- config/outer-keys.h
+++ config/outer-keys.h
@@ -45,7 +45,7 @@
 #define LT_2    &lt NAV_L LTK_2
 #define LT_3    &lt PHP_L LTK_3
 #define LT_4    &none   // ENC   LTK_4
-#define LT_2_1  &tog DK_HRM_L // &kp LTK_2_1 // &lt LTL_2_1  LTK_2_1
+#define LT_2_1  &kp    LTK_2_1 // &lt LTL_2_1  LTK_2_1
 //
 #define RT_1    &kp    RTK_1
 #define RT_2    &kp    RTK_2
