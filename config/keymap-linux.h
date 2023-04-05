c36062d9b94ca91c592872ea3abf10ef8f2ccc98
Manuel Transfeld <auipga@gmail.com>
2023-04-05 16:32:04 +0200

    Update encoder: brightness, tab switching

--- config/keymap-linux.h
+++ config/keymap-linux.h
@@ -6,6 +6,10 @@
 #define U_ZOOM     &kp LG(LS(DE_Z)) // Large Text toggle (Zoom)
 #define U_PROMPT   &kp LA(F2)       // Alt-F2 prompt (+LG=restart gnome shell)
 #define U_SWI_POP  &kp LG(W)        // Window Switcher Popup (like Alt-Tab, but stays open)
+#define u_TAB_NXT  LC(TAB)          // Next Tab      (for sensor-bindings)
+#define u_TAB_PRV  LS(LC(TAB))      // Previous Tab  (for sensor-bindings)
+#define U_TAB_NXT  &kp u_TAB_NXT    // Next Tab
+#define U_TAB_PRV  &kp u_TAB_PRV    // Previous Tab
 
 #define YT_FASTER  &kp LS(DE_DOT)
 #define YT_SLOWER  &kp LS(DE_COMMA)
@@ -14,5 +18,8 @@
 #define U_CONSOL  &kp LG(T)         // gnome-terminal
 #define U_ADRBAR  &kp LC(L)         // focus address bar (Firefox, Nautilus)
 #define U_SYSMON  &kp LC(LS(ESC))   // gnome-system-monitor
-#define u_BRI_UP  LG(LC(UP))        // bash -c "ddcutil setvcp 10 +10"
-#define u_BRI_DN  LG(LC(DOWN))      // bash -c "ddcutil setvcp 10 -10"
+
+// gnome-shell extension | Brightness control using ddcutil
+// https://extensions.gnome.org/extension/2645/brightness-control-using-ddcutil/
+#define u_BRI_UP  LC(C_BRI_UP)      // bash -c "ddcutil setvcp 10 +10"
+#define u_BRI_DN  LC(C_BRI_DN)      // bash -c "ddcutil setvcp 10 -10"
